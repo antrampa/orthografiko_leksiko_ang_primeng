b57c13fed8835fe6efb4f25c42c8311461e931f9
Antonis Zalonis <antonis.zalonis@gmail.com>
2022-12-01 23:02:31 +0100

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {ButtonModule} from 'primeng/button';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { WordListComponent } from './components/word-list/word-list.component';
+import { WordAddEditComponent } from './components/word-add-edit/word-add-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    WordListComponent,
+    WordAddEditComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/word-add-edit/word-add-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-word-add-edit',
+  templateUrl: './word-add-edit.component.html',
+  styleUrls: ['./word-add-edit.component.scss']
+})
+export class WordAddEditComponent {
+
+}
